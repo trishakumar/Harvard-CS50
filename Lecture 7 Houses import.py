2e62e5c0a96917fde1e9ae8080206ff5bd16e093
trishakumar <58877381+trishakumar@users.noreply.github.com>
2020-05-11 19:30:00 +0530

    Create Lecture 7 Houses import.py

---
+++ Lecture 7 Houses import.py
@@ -0,0 +1,45 @@
+import sqlite3
+import csv
+import sys
+
+
+def main():
+
+    if (len(sys.argv) != 2):
+        sys.exit("Usage: import.py file.csv")
+
+    filename = sys.argv[1]
+
+    if not (filename.endswith(".csv")):
+        sys.exit("You must provide a *.csv")
+
+    sqlite_file = "students.db"
+    con = sqlite3.connect(sqlite_file)
+
+    cur = con.cursor()
+
+    with open(filename, "r") as characters:
+
+        reader = csv.DictReader(characters)
+
+        for row in reader:
+            names = []
+
+            for part in row["name"].split(" "):
+                names.append(part)
+
+            names.append(row["house"])
+            names.append(row["birth"])
+
+            if (len(names) == 5):
+                cur.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", names[:5])
+
+            if (len(names) == 4):
+                cur.execute("INSERT INTO students (first, last, house, birth) VALUES(?, ?, ?, ?)", names[:4])
+
+    con.commit()
+    con.close()
+
+
+if __name__ == "__main__":
+    main()
